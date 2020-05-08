fcd3ffdf1fdaaa056d38f5922bb0e7f37d27f325
cankahramanm <60780482+cankahramanm@users.noreply.github.com>
2020-05-08 22:33:26 +0300

    Update tictactoe.c

--- tictactoe.c
+++ tictactoe.c
@@ -3,15 +3,16 @@
 
 
 
-int main(){
-    int i,j=0;
+int main()
+{
+    int i,j;
     char root[3][3]={{'-','-','-'},{'-','-','-'},{'-','-','-'}};// ilk çıkacak şekil
-    printf("1 2 3\n"); 
+    printf("0 1 2\n"); 
     for(i=0;i<3;i++){
         printf("%d",i);
             for(j=0;j<3;j++){
-                printf(" %c ",root[i][j]);
- }
-
+                printf(" %c\n ",root[i][j]);
+                            }
+                    }
     return 0;
 }
